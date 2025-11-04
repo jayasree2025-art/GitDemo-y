76042cb18b92eddb3573089b9cc408e899623e1b
jayasree2025-art <jayasree17@gmail.com>
2025-11-04 14:59:38 +0530

    Renamed day1.java to D1.java and updated code

---
+++ src/main/resources/TEST/D1.java
@@ -0,0 +1,16 @@
+package TestNGFrameworks;
+
+import org.testng.annotations.Test;
+
+public class Section1 {
+	
+	@Test
+	public void Demo() {
+		System.out.println("Hello");
+		System.out.println("jayasree");
+		System.out.println("baneet");
+
+		
+	}
+
+}
